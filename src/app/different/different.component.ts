cd14726af226d53ec32a31f373d8f5d6c11c8001
Nishanth Reddy <nishanth.reddym004@gmail.com>
2026-03-02 16:54:49 +0530

    svg icons fix 1.3

--- src/app/different/different.component.ts
+++ src/app/different/different.component.ts
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
 
 interface DifferentFeature {
 
-  iconSrc: string;
-
   label: string;
 
 }
@@ -34,15 +32,11 @@ export class DifferentComponent {
 
     {
 
-      iconSrc: 'assets/icons/different/guest-is-god.svg',
-
       label: 'Guest is GOD Philosophy',
 
     },
 
     {
-
-      iconSrc: 'assets/icons/different/clean-spaces.svg',
 
       label: 'Clean Spaces',
 
@@ -50,15 +44,11 @@ export class DifferentComponent {
 
     {
 
-      iconSrc: 'assets/icons/different/near-it-corridor.svg',
-
       label: 'Near IT Corridor',
 
     },
 
     {
-
-      iconSrc: 'assets/icons/different/food-like-home.svg',
 
       label: 'Food like at Home',
 
@@ -66,15 +56,11 @@ export class DifferentComponent {
 
     {
 
-      iconSrc: 'assets/icons/different/community-focused.svg',
-
       label: 'Community Focused',
 
     },
 
     {
-
-      iconSrc: 'assets/icons/different/sustainable-ops.svg',
 
       label: 'Sustainable Operations',
 
@@ -82,23 +68,17 @@ export class DifferentComponent {
 
     {
 
-      iconSrc: 'assets/icons/different/smart-technology.svg',
-
       label: 'Smart Technology',
 
     },
 
     {
 
-      iconSrc: 'assets/icons/different/concierge.svg',
-
       label: '24/7 Concierge',
 
     },
 
     {
-
-      iconSrc: 'assets/icons/different/aesthetic-interiors.svg',
 
       label: 'Aesthetically Designed Interiors',
 
