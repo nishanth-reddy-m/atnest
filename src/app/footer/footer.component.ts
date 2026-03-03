d071a54f5adba2a263826018d29609df5c6804dc
Nishanth Reddy <nishanth.reddym004@gmail.com>
2026-03-03 17:44:53 +0530

    footer icons bug fix

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,19 +8,4 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-  readonly newsletterIconSrc = 'https://www.figma.com/api/mcp/asset/22bd0d45-f838-470f-8570-8cde133dfcca';
-  readonly emailIconSrc = 'https://www.figma.com/api/mcp/asset/acec94da-1cea-4c74-a3b4-76511a138a21';
-  readonly arrowIconSrc = 'https://www.figma.com/api/mcp/asset/a5178afc-6d7a-4e8e-a7f2-ced3cf65d58e';
-  readonly logoSrc = 'https://www.figma.com/api/mcp/asset/4f1e3953-9d6f-4d8c-98f9-2f2adf2519e3';
-  readonly phoneIconSrc = 'https://www.figma.com/api/mcp/asset/7b8763b5-4659-4302-a095-7cb213f3d4d0';
-  readonly emailContactIconSrc = 'https://www.figma.com/api/mcp/asset/7229c57b-0194-45af-8f73-63538ddbfba4';
-  readonly locationIconSrc = 'https://www.figma.com/api/mcp/asset/bdf75a39-b4fa-4c92-953b-e68ad61fbf2c';
-  readonly facebookIconSrc = 'https://www.figma.com/api/mcp/asset/b0a4d851-d82f-44ef-8607-8eae0c126f3f';
-  readonly twitterIconSrc = 'https://www.figma.com/api/mcp/asset/74da799f-40b0-464c-889d-c417f41c592d';
-  readonly instagramIconSrc = 'https://www.figma.com/api/mcp/asset/9a924755-77fd-417d-84b6-45dc422df3d6';
-  readonly linkedinIconSrc = 'https://www.figma.com/api/mcp/asset/3aaf3466-3cd5-45de-8221-43bf4c13bd58';
-  readonly youtubeIconSrc = 'https://www.figma.com/api/mcp/asset/c3307ce8-6f2d-4d7b-9a8e-b493df363f2b';
-  readonly heartIconSrc = 'https://www.figma.com/api/mcp/asset/06dca3f4-120f-4a74-aef5-511cbea0299c';
-  readonly secureIconSrc = 'https://www.figma.com/api/mcp/asset/aae9cf77-a94d-4f82-841b-c73e9e91646d';
-  readonly flagIconSrc = 'https://www.figma.com/api/mcp/asset/822863cb-ae9a-42df-a097-510b2ee1cb53';
 }
