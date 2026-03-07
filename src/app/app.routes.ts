4470efa6bf26ddbe9331c46e87f630a82a04d961
Nishanth Reddy <nishanth.reddym004@gmail.com>
2026-03-07 14:54:29 +0530

    Properties route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PropertiesComponent } from './properties/properties.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'ATNEST - Premium Hotels'
+  },
+  {
+    path: 'properties',
+    component: PropertiesComponent,
+    title: 'ATNEST - Properties'
   },
   {
     path: '**',
---
+++ src/app/properties/properties.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-properties',
+  standalone: true,
+  templateUrl: './properties.component.html',
+  styleUrl: './properties.component.css',
+})
+export class PropertiesComponent {}
